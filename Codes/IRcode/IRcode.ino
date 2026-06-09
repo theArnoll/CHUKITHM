8350b776abb0fe8b8c51b4b3efa58026774718ef
theArnoll <40510737+theArnoll@users.noreply.github.com>
2026-06-09 09:03:59 +0800

    Test SPI, regenerate IR code, complete PCB design

--- Codes/IRcode/IRcode.ino
+++ Codes/IRcode/IRcode.ino
@@ -1,28 +1,49 @@
-#define IR_TX_PIN 4  // IR emission (+220Ω resistor)
-#define IR_RX_PIN 5  // The OUT pin of IRM-3638
+#define IR_TX_PIN 4  
+
+unsigned long lastResetTime = 0;
 
 void setup() {
-	// pinMode(IR_RX_PIN, INPUT);
-	for (uint8_t re = 8; re <= 13; re++)
+	for (uint8_t re = 5; re <= 10; re++) {
 		pinMode(re, INPUT);
+	}
+	
+	pinMode(IR_TX_PIN, OUTPUT);
+	pinMode(LED_BUILTIN, OUTPUT);
 	Serial.begin(115200);
 }
 
 void loop() {
 	tone(IR_TX_PIN, 38000); 
 	
-	delayMicroseconds(500); 
+	// 保持極短的發射，減少能量累積
+	delayMicroseconds(300); 
 
-	int rx_state[6];
-	for (uint8_t re = 8; re <= 13; re++)
-		rx_state[re - 8] = digitalRead(re);
+	int rx_state[6] = {0};
+	for (uint8_t re = 5; re <= 10; re++) {
+		rx_state[re - 5] = digitalRead(re);  
+	}
 
-	for (uint8_t re = 8; re <= 13; re++)
-		noTone(re);
+	noTone(IR_TX_PIN);
 
-	for (uint8_t re = 0; re < 6; re++)
-	{
-		Serial.print(rx_state[re]);
+	for (uint8_t i = 0; i < 6; i++) {
+		Serial.print(rx_state[i]);
 		Serial.print(" | ");
-	}   Serial.println();
+	}
+	Serial.print("Booted ");
+	Serial.print(millis());
+	Serial.print(" ms");
+	Serial.println();
+
+	// 【最後的黑魔法：週期性長休法】
+	unsigned long currentTime = millis();
+	
+	// 如果已經連續高頻率掃描了 25 毫秒
+	if (currentTime - lastResetTime > 25) {
+		// 強制給予 15 毫秒的超級長假，讓 AGC 徹底重置歸零！
+		delay(15);
+		lastResetTime = millis();
+	} else {
+		// 正常的高頻率掃描只休息 2 毫秒
+		delay(2);
+	}
 }
--- Codes/forCY8C/CY8C_code/CY8C_code.ino
+++ Codes/forCY8C/CY8C_code/CY8C_code.ino
@@ -1,34 +1,48 @@
 #include <CypressCY8CMBR3116.h>
 #include <Wire.h>
-#include "NKRO_TinyUSB.h" // 【修改 1】換成我們的 TinyUSB NKRO 標頭檔
-
+#include "NKRO_TinyUSB.h" // TinyUSB NKRO
 #include <Adafruit_NeoPixel.h>
+#include <SPI.h>
 
 #define I2C_ADDRESS 0x51     //I2C Address of the Cypress IC (0x37 is default)
 #define REQUEST_TIMEOUT 40   //After how many request is a Timeout triggered
 
 CY8CMBR3116 touchIC(I2C_ADDRESS, REQUEST_TIMEOUT);
-
 Adafruit_NeoPixel pixels(1, 16, NEO_GRB + NEO_KHZ800);
 
-void sendSPI(uint8_t latchPin, uint8_t dataPin, uint8_t clockPin, uint8_t data) {
+void sendSPI(uint32_t data, uint8_t latchPin, uint8_t dataLength) {
+    SPI1.beginTransaction(SPISettings(1000000, MSBFIRST, SPI_MODE0));
     digitalWrite(latchPin, LOW);
-    shiftOut(dataPin, clockPin, MSBFIRST, data);
+    switch (dataLength) {  // 1~4 or 8, 16, 24, 
+        case 1: case 8:
+            SPI1.transfer(data & 0xFF);
+            break;
+        case 2: case 16:
+            SPI1.transfer16(data & 0xFFFF);
+            break;
+        case 3: case 24:
+            SPI1.transfer((data >> 16) & 0xFF);
+            SPI1.transfer16(data & 0xFFFF);
+            break;
+        case 4: case 32:
+            SPI1.transfer16((data >> 16) & 0xFFFF);
+            SPI1.transfer16(data & 0xFFFF);
+            break;
+        default: 
+            break;
+    }
     digitalWrite(latchPin, HIGH);
+    SPI1.endTransaction();
 }
 
 void setup() {
     Serial.begin(250000);
-    
-    pixels.begin();
-    pixels.setBrightness(10);
+    pixels.begin(); pixels.setBrightness(10);
     
     Wire.setSDA(0); Wire.setSCL(1);
     Wire.begin();   
     Wire.setClock(400000);  // 400kHz I²C fast mode
 
-    pinMode(10, OUTPUT);
-    digitalWrite(10, 0);
     pinMode(7, INPUT_PULLUP);
     pinMode(8, INPUT_PULLUP);
     
@@ -47,9 +61,19 @@ void setup() {
     if (Wire.endTransmission() == 0) Serial.println("IC 1 found");
     Wire.beginTransmission(I2C_ADDRESS + 1);
     if (Wire.endTransmission() == 0) Serial.println("IC 2 found");
-    delay(50);  // 50ms prepare wake time to wake up the IC.
+
+    SPI1.setSCK(10); SPI1.setTX(11);
+    SPI1.begin();
+    pinMode(12, OUTPUT);
 
     if(!digitalRead(8)) KB.begin();
+
+    for (int i = 0; i <= 16; i++) {
+        uint16_t marqueeData = (1 << i);
+        sendSPI(marqueeData, 12, 2);
+        delay(100);
+    }
+
     Serial.println("Start Program");
 }
 
@@ -60,6 +84,8 @@ char keymap[2][2][8] = {  //   0    1    2    3    4    5    6    7
                             { '0', '0', '0', '0', '0', '0', '0', '0' }   } };
 uint8_t lastStatus[2][2] = { { 0, 0 }, { 0, 0 } }; // 2 ICs
 
+unsigned long lastmicroS = micros();
+
 void loop() {
     uint8_t touchStatus[2][2];
     requestTouchStatus(touchStatus);
@@ -67,10 +93,8 @@ void loop() {
     if(digitalRead(7) == LOW)
     {
         Serial.print("|");
-
         for (uint8_t re = 0; re < 2; re++)
-        {
-            for (int pointer = 0; pointer < 8; pointer++)
+        {   for (int pointer = 0; pointer < 8; pointer++)
             {
                 uint8_t pressed = touchStatus[0][re] >> pointer & 0b00000001;
                 Serial.print((char)(pressed ? (pointer+'0') : '-'));
@@ -103,26 +127,16 @@ void loop() {
 
     KB.send();
     lastStatus[0][0] = touchStatus[0][0]; lastStatus[0][1] = touchStatus[0][1];
+	Serial.print(micros() - lastmicroS);
+	lastmicroS = micros();
 }
 
 void requestTouchStatus(uint8_t statusStorage[2][2])
 {
     uint8_t touchStatusBuffer[2];
     uint8_t error = touchIC.get_BUTTON_STAT(touchStatusBuffer);
-    if (error != 0)
-    {
-        Serial.print("Request Touch Status Loop: ");
-        printError(error);
-        switch(error)
-        {
-            case 1: pixels.setPixelColor(0, pixels.Color(255, 0, 0)); pixels.show(); break; // red
-            case 2: pixels.setPixelColor(0, pixels.Color(0, 255, 0)); pixels.show(); break; // green
-            case 3: pixels.setPixelColor(0, pixels.Color(0, 0, 255)); pixels.show(); break; // blue
-            case 4: pixels.setPixelColor(0, pixels.Color(255, 255, 0)); pixels.show(); break; // yellow
-            case 5: pixels.setPixelColor(0, pixels.Color(255, 0, 255)); pixels.show(); break; // purple
-            default: pixels.setPixelColor(0, pixels.Color(255, 255, 255)); pixels.show(); break; // white
-        }
-    } else { pixels.clear(); pixels.show(); }
+    if (error != 0) { Serial.print("Request Touch Status Loop: "); printError(error); }
+                else { pixels.clear(); pixels.show(); }
     statusStorage[0][0] = touchStatusBuffer[0]; statusStorage[0][1] = touchStatusBuffer[1];
 }
 
@@ -132,14 +146,20 @@ void printError(uint8_t errorCode)
     Serial.print("Communication Error ");
     switch (errorCode)
     {
-        case 1: Serial.println("1: Data too long to fit in transmit buffer"); break;
-        case 2: Serial.println("2: Received NACK on transmit of address"); break;
-        case 3: Serial.println("3: Received NACK on transmit of data"); break;
-        case 4: Serial.println("4: Other Error"); break;
-        case 5: Serial.println("5: Timeout Error"); break;
+        case 1: Serial.println("1: Data too long to fit in transmit buffer");
+                pixels.setPixelColor(0, pixels.Color(255, 0, 0)); pixels.show(); break; // red
+        case 2: Serial.println("2: Received NACK on transmit of address"); 
+                pixels.setPixelColor(0, pixels.Color(0, 255, 0)); pixels.show(); break; // green
+        case 3: Serial.println("3: Received NACK on transmit of data"); 
+                pixels.setPixelColor(0, pixels.Color(0, 0, 255)); pixels.show(); break;  // blue
+        case 4: Serial.println("4: Other Error");
+                pixels.setPixelColor(0, pixels.Color(255, 255, 0)); pixels.show(); break; // yellow
+        case 5: Serial.println("5: Timeout Error"); 
+                pixels.setPixelColor(0, pixels.Color(255, 0, 255)); pixels.show(); break; // purple
         default:
             Serial.print("Unknown: Unknown Wire Error ");
             Serial.println(errorCode);
+            pixels.setPixelColor(0, pixels.Color(255, 255, 255)); pixels.show();
             break;
     }
 }
